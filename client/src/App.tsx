924ec41d0b0b7003ebc93f4566919bf782b4c55d
Gedeon Nzemba <40776419+GedeonNzemba@users.noreply.github.com>
2022-07-06 11:06:49 +0200

    setup done
    finished setting up react app with typescript and installing and configuring tailwing css

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GradientText from './components/GradientText';
+
+function App() {
+  return (
+    <GradientText text='Testing one Two Three Four AND MORE' />
+  );
+}
+
+export default App;
