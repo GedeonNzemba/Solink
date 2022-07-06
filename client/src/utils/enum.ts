863f5809e6005836afaf9fab6c1345cccd1e78e9
Gedeon Nzemba <40776419+GedeonNzemba@users.noreply.github.com>
2022-07-06 11:35:21 +0200

    added enum file
    enum file created to store all string values such as colors

---
+++ client/src/utils/enum.ts
@@ -0,0 +1,7 @@
+export enum ColorSchemaName {
+    SOLINK_GREEN = '#114B5F',
+    SOLINK_BLUE = '#456990',
+    SOLINK_NYANZA = '#E4FDE1',
+    SOLINK_ROSE = '#F45B69',
+    SOLINK_WHITE = '#6B2737'
+}
